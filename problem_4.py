ba1a171b157401b156c724957174104c013898b4
Daniel Dao <danielvudao@utexas.edu>
2014-11-20 18:02:12 -0600

    problem 4

---
+++ problem_4.py
@@ -0,0 +1,29 @@
+def problem_four():
+    max_product = 0 
+    
+    for i in xrange(100, 1000):
+        for j in xrange(100, 1000):
+            cur = i * j
+            if is_palindrome(cur) and cur > max_product:
+                max_product = cur
+
+    print max_product
+
+
+
+
+def is_palindrome(num):
+    str_num = str(num)
+    beg = 0
+    end = len(str_num) - 1
+
+    while beg <= end:
+        if str_num[beg] != str_num[end]:
+            return False
+        beg += 1
+        end -= 1
+
+    return True
+
+if __name__ == '__main__':
+    problem_four()
